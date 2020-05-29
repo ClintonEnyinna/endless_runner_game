c2b0d940a07bd60a8f04f1dab3d895bfe1547d32
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-05-29 12:22:45 -0500

    create an obstacle class for making new obstacles

---
+++ src/Objects/Obstacle.js
@@ -0,0 +1,13 @@
+import 'phaser';
+
+export default class Obstacle extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, key) {
+    super(scene, x, y, key);
+
+    this.setScale(0.15);
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.setGravityY(500);
+  }
+}
