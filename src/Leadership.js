0c6b98c35925d89f1533769d57a9af7b94d46dd8
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-05-29 20:26:02 -0500

    feat: leaderboard api get and post requests functions

---
+++ src/Leadership.js
@@ -0,0 +1,31 @@
+const baseUrl =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+
+const id = 'wiwuz8XPfcvf2MG5o2fz';
+
+const postData = async (playerScore) => {
+  const response = await fetch(`${baseUrl}/${id}/scores`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(playerScore),
+  });
+
+  const result = await response.json();
+  const data = await result;
+
+  return data;
+};
+
+const getData = async () => {
+  const response = await fetch(`${baseUrl}/${id}/scores`);
+
+  const result = await response.json();
+  const data = await result;
+
+  return data;
+};
+
+export { postData, getData };
